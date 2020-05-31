82f2a541c41e95535b4cc2c1bdff82c2965113f9
Muhamad Fakhrusy <fakhrusy.m@gmail.com>
2020-05-31 19:19:43 +0700

    update styling + import mock data for quiz screen

--- src/screens/QuizScreen/index.tsx
+++ src/screens/QuizScreen/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Layout from '../../components/Layout';
+import styled from 'styled-components';
+
+// import only, still unused
+import { mockData } from './mock';
+
+const Container = styled.div`
+  flex-grow: 1;
+`;
 
 const QuizScreen = () => {
   return (
     <Layout>
-      <div>
+      <Container>
         quiz!
-      </div>
+      </Container>
     </Layout>
   )
 }
